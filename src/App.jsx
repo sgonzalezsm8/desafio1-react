f788a016202d08ec0612979ca4e014d86c43eb2d
Sergio Gonzalez <sgonzalezsm8@gmail.com>
2024-08-10 18:49:11 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+     </div>
+    </>
+  )
+}
+
+export default App
